67f9ac841cb8e865a64beee0641dcadf430ffd40
Nelson Leung <nelson.leung@dyn-160-39-198-182.dyn.columbia.edu>
2019-10-02 14:55:38 -0400

    eject

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,1 +1,9 @@
-hello world
+import flask
+
+app = flask.Flask("__main__")
+
+@app.route("/")
+def my_index():
+    return flask.render_template("index.html", token = "Hello Flask+ React")
+
+app.run(debug=True)
