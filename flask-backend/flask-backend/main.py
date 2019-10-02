383182ccccefe45ad300bc639eafb758086a4330
Nelson Leung <nelson.leung@dyn-160-39-198-182.dyn.columbia.edu>
2019-10-02 15:41:20 -0400

    flask-react-app-build-foundation

--- flask-backend/flask-backend/main.py
+++ flask-backend/flask-backend/main.py
@@ -4,6 +4,6 @@ app = flask.Flask("__main__")
 
 @app.route("/")
 def my_index():
-    return flask.render_template("index.html", token = "Hello Flask+ React")
+    return flask.render_template("index.html", token="Hello Flask+ React")
 
 app.run(debug=True)
